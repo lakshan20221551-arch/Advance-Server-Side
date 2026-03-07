e303c29a7404727e3c83db9ffd1c2fe60b2ab5cd
LakshanW <lakshanw@sagesystems.lk>
2026-03-07 17:16:03 +0530

    create files and install dependancies

---
+++ CW_AS/config/db.js
@@ -0,0 +1,22 @@
+const sql = require('mssql');
+require('dotenv').config();
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: {
+        trustServerCertificate: true
+    }
+};
+
+const pool = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log("Connected to SQL Server");
+        return pool;
+    })
+    .catch(err => console.log(err));
+
+module.exports = { sql, pool };
